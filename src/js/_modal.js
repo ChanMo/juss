ac9255f0e09c7c294408c26e2de26bdda60e1279
ChanMo <chen.orange@aliyun.com>
2016-12-06 10:38:19 +0800

    fixed modal

--- src/js/_modal.js
+++ src/js/_modal.js
@@ -1,16 +1,5 @@
 $("[data-modal-toggle]").bind("click", function(e){
   e.preventDefault();
   $(".modal-bg").toggle();
-  if($(".modal").hasClass("modal-active")){
-    $(".modal").toggleClass("modal-active");
-    window.setTimeout(function(){
-      $(".modal").hide();
-    },400);
-  }else{
-    console.log('show');
-    $(".modal").show();
-    window.setTimeout(function(){
-    $(".modal").toggleClass("modal-active");
-    }, 0);
-  };
+  $(".modal").toggleClass("modal-active");
 });
