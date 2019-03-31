316c38efec21e5f2716ac083e4a9cd89a38448a7
dyou <chen.orange@aliyun.com>
2019-03-31 20:37:28 +0800

    remove old code

--- docs/js/_modal.js
+++
@@ -1,5 +0,0 @@
-$("[data-modal-toggle]").bind("click", function(e){
-  e.preventDefault();
-  $(".modal-bg").toggle();
-  $(".modal").toggleClass("modal-active");
-});
--- src/js/_offcanvas.js
+++
@@ -1,6 +0,0 @@
-$("[data-off-canvas-toggle]").bind("click", function(e){
-  e.preventDefault();
-  var ele = $(".off-canvas");
-  ele.toggleClass("off-canvas_active");
-  ele.find(".off-canvas__bg").toggle();
-});
